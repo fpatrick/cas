ecf5a8a446062061b12ad5387ad5c89dc6afdb0a
erick <fpatrick.erick@gmail.com>
2022-03-18 19:24:46 +0000

    Fix file datetime creation

--- cas.py
+++ cas.py
@@ -6,7 +6,7 @@ def start():
     menu_choice = 99
     print()
     print("***********************************************************************")
-    print("*              Welcome to CaS - Command and Script v0.8.6             *")
+    print("*              Welcome to CaS - Command and Script v0.8.7             *")
     print("* Dependency: Run with sudo. Install screen (sudo apt install screen) *")
     print("* by https://github.com/fpatrick/cas                          and dxT *")
     print("***********************************************************************")
@@ -53,9 +53,10 @@ def make_file(user_folder, user_command):
             f.write('import subprocess\n')
             f.write('import os.path\n')
             f.write('from datetime import datetime\n')
+            f.write('now = datetime.now()\n')
             f.write('dt_string = now.strftime("%d/%m/%Y %H:%M:%S")\n')
             f.write('if os.path.isfile("' + lock + '"):\n')
-            f.write('    print(f"{dt_string}: Lock found, the command is still running. Delete lock file to force run.")\n')
+            f.write('    print(f"{dt_string}: ' + script_name + '.lock found, the command is still running. Delete lock file to force run.")\n')
             f.write('else:\n')
             f.write('    subprocess.run("' + screen_command + '", shell=True, capture_output=True, text=True)\n')
 
@@ -88,7 +89,7 @@ if menu == 1:
     print(">>> * Info: Your screen section may remain open when command finish. (screen -list  and then  screen kill <number>) *")
     print()
     print(result.stdout)
-    input("\n\nEnter any key to exit. ")
+    input("\n\nPress enter to exit. ")
 
 # If choice is to create a script
 elif menu == 2:
@@ -103,9 +104,9 @@ elif menu == 2:
     print("> Enter on terminal: sudo crontab -e")
     print("> Write on same line: <schedule> <python3 path> <script full path>  <optional log>")
     print(f"> Example: * * * * * /usr/bin/python3 /folders/{fullPath} >> ~/cron.log 2>&1 \n\n")
-    print("> *!* ATTENTION *!* recreate the script if you need to move to another folder!!! \n\n")
+    print("> *!* ATTENTION *!* recreate the script if you need to move it to another folder!!! \n\n")
 
-    input("Enter any key to exit. ")
+    input("Press enter to exit. ")
 
 elif menu == 0:
     exit()
