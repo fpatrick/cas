e5f7d527d76e098b958c343efaf7145f40489eb8
erick <fpatrick.erick@gmail.com>
2022-03-18 19:06:28 +0000

    Fix remove of file and not stop using close_program

--- cas.py
+++ cas.py
@@ -3,6 +3,7 @@ import subprocess
 
 # Initial function to loop valid options
 def start():
+    menu_choice = 99
     print()
     print("***********************************************************************")
     print("*              Welcome to CaS - Command and Script v0.8.5             *")
@@ -25,7 +26,7 @@ menu = start()
 
 # Make folder to store scripts
 def make_folder():
-    print(">>> Please enter full path where the script will be created (ex: /example/yourfolder):")
+    print(">>> Please enter *!* FULL PATH *!* where the script will be created (ex: /example/yourfolder):")
     folder_path = input()
     command = "mkdir -p " + folder_path
     result_command = subprocess.run(command, shell=True, capture_output=True, text=True)
@@ -42,7 +43,7 @@ def make_file(user_folder, user_command):
     print(">>> What will be the script name?")
     script_name = input()
     create_lock = "touch " + user_folder + "/" + script_name + ".lock && "
-    remove_lock = " && rm -f " + user_folder + "/" + script_name + ".lock"
+    remove_lock = " ; rm -f " + user_folder + "/" + script_name + ".lock"
     screen_command = "screen -dmS section_created_by_script  bash -c " + "'" + create_lock + user_command + remove_lock + "'"
 
     lock = user_folder + "/" + script_name + ".lock"
@@ -51,8 +52,10 @@ def make_file(user_folder, user_command):
         with open(f_path, 'w') as f:
             f.write('import subprocess\n')
             f.write('import os.path\n')
+            f.write('from datetime import datetime\n')
+            f.write('dt_string = now.strftime("%d/%m/%Y %H:%M:%S")\n')
             f.write('if os.path.isfile("' + lock + '"):\n')
-            f.write('    print("Lock found. The command is still running.")\n')
+            f.write('    print(f"{dt_string}: Lock found, the command is still running. Delete lock file to force run.")\n')
             f.write('else:\n')
             f.write('    subprocess.run("' + screen_command + '", shell=True, capture_output=True, text=True)\n')
 
@@ -85,7 +88,7 @@ if menu == 1:
     print(">>> * Info: Your screen section may remain open when command finish. (screen -list  and then  screen kill <number>) *")
     print()
     print(result.stdout)
-    menu = close_program()
+    input("\n\nEnter any key to exit. ")
 
 # If choice is to create a script
 elif menu == 2:
@@ -100,7 +103,9 @@ elif menu == 2:
     print("> Enter on terminal: sudo crontab -e")
     print("> Write on same line: <schedule> <python3 path> <script full path>  <optional log>")
     print(f"> Example: * * * * * /usr/bin/python3 /folders/{fullPath} >> ~/cron.log 2>&1 \n\n")
-    close_program()
+    print("> *!* ATTENTION *!* recreate the script if you need to move to another folder!!! \n\n")
+
+    input("Enter any key to exit. ")
 
 elif menu == 0:
     exit()
